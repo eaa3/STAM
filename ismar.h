52d16095a55c6df403bd5a01e6dcf34fbadff28a
Ermano Arruda <ermanoarruda@Ermanos-MacBook-Pro-418.local>
2015-08-04 19:15:35 +0100

    setting allall up

---
+++ ismar.h
@@ -0,0 +1,53 @@
+﻿// ==============================================================================
+// If you have further question, feel free to email tracking_chairs@ismar15.org.
+// ==============================================================================
+//
+// In thie “ismar.h”,
+// please set the number of templates N.
+// please set the number of frames L of the video sequence.
+//
+// ==============================================================================
+
+// ---------------------------------------------------------------
+// include OpenCV libs
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+// for the MSVC
+#define CV_VERSION_STR CVAUX_STR(CV_MAJOR_VERSION) CVAUX_STR(CV_MINOR_VERSION) CVAUX_STR(CV_SUBMINOR_VERSION)
+#ifdef _DEBUG
+#define CV_EXT_STR "d.lib"
+#else
+#define CV_EXT_STR ".lib"
+#endif
+
+#pragma comment (lib, "C:\\Program Files\\opencv\\build\\x86\\vc12\\lib\\opencv_core" CV_VERSION_STR CV_EXT_STR)
+#pragma comment (lib, "C:\\Program Files\\opencv\\build\\x86\\vc12\\lib\\opencv_highgui" CV_VERSION_STR CV_EXT_STR)
+#pragma comment (lib, "C:\\Program Files\\opencv\\build\\x86\\vc12\\lib\\opencv_imgproc" CV_VERSION_STR CV_EXT_STR)
+
+// for using OpenCV3.0, this code is necessary.
+//#pragma comment (lib, "C:\\Program Files\\opencv\\build\\x86\\vc12\\lib\\opencv_world" CV_VERSION_STR CV_EXT_STR)
+// ---------------------------------------------------------------
+
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+
+using namespace cv;
+using namespace std;
+
+// remove warnings
+#pragma warning (disable : 4018)
+#pragma warning (disable : 4244)
+
+// Set the number of templates N (default 40).
+#define N 40
+
+// Set the number of frames L of the video sequence (default 201).
+#define M 201
+
+// Competitors edit this function. 
+void user_function(Mat search_img, Mat pM);
---
+++ main2.cpp
@@ -0,0 +1,52 @@
+﻿// ==============================================================================
+// ISMAR 2015 Tracking Competition 
+// 
+// main2.cpp 
+// Input: A reference image "S01L02_VGA_****.png"
+// Output: A Projection Matrix "S01L02_Matrix.csv"
+// 
+// <Operating environment>
+// The compilation is checked on the following environments:
+// - Windows8.1 + OpenCV 2.4.10, OpenCV 3.0.0
+//
+// If you have further question, feel free to email tracking_chairs@ismar15.org.
+// ==============================================================================
+
+#include "ismar.h"
+
+int main(int argc, char *argv[])
+{
+	cout << "--------------------------------" << endl;
+	cout << "ISMAR 2015  Tracking Competition" << endl;
+	cout << "  Scenario 01  Level 02" << endl;
+	cout << "--------------------------------" << endl;
+
+	for (int k = 0; k < M; k++)
+	{
+		cout << "frame number = " << setfill('0') << setw(4) << right << k << endl;
+
+		// --------------------------------------
+		// Read "S01L02_VGA_****.png" as a reference image.
+		char buf[256];
+		sprintf(buf, "S01L02_VGA/S01L02_VGA_%04d.png", k);
+		Mat search_img = imread(buf);
+
+		// When the image is not available, quite this program.
+		if (search_img.empty())
+		{
+			cout << "Error: Can't find the search image file." << endl;
+			return -1;
+		}
+		// --------------------------------------
+
+		Mat pM(3, 4, CV_64FC1);
+
+		user_function(search_img, pM);
+
+		// Output the estimated projection matrix at the last frame.
+		ofstream ofs("S0102_Matrix.csv", ios::trunc);
+		for (int i = 0; i < 3; i++)
+			ofs << pM.at<double>(i, 0) << "," << pM.at<double>(i, 1) << "," << pM.at<double>(i, 2) << "," << pM.at<double>(i, 3) << endl;
+	}
+	return 0;
+}
