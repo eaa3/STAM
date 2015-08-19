0a0d8111a4130a1f8fb6867b9550f6283e5d4b2f
Ermano Arruda <ermanoarruda@Ermanos-MacBook-Pro-418.local>
2015-08-19 18:21:53 +0100

    added generic matcher

--- include/utils.h
+++ include/utils.h
@@ -11,6 +11,85 @@ namespace utils {
 // Matcher goes here
 class GenericMatcher {
 
+
+    typedef std::vector<cv::DMatch> MatchSeq;
+
+    GenericMatcher() : ratio_(0.65f), refineF_(true),
+        confidence_(0.99), distance_(3.0) {
+        // SURF is the default feature
+        detector_ = new cv::StarFeatureDetector(32, 10, 18, 18, 20);//new cv::SurfFeatureDetector();
+        extractor_ = new cv::SURF(1000, 4, 2, false, true);//new cv::SurfDescriptorExtractor();
+    }
+
+    GenericMatcher(float ratio, double confidence, double distance, bool refineF = true) : ratio_(ratio), refineF_(refineF),
+        confidence_(confidence), distance_(distance) {
+        // SURF is the default feature
+        detector_ = new cv::StarFeatureDetector(32, 10, 18, 18, 20);//new cv::SurfFeatureDetector();
+        extractor_ = new cv::SURF(1000, 4, 2, false, true);//new cv::SurfDescriptorExtractor();
+    }
+
+
+    // Set the feature detector
+    void setFeatureDetector(
+            cv::Ptr<cv::FeatureDetector>& detect) {
+        detector_ = detect;
+    }
+
+    // Set the descriptor extractor
+    void setDescriptorExtractor(
+            cv::Ptr<cv::DescriptorExtractor>& desc) {
+        extractor_ = desc;
+    }
+
+
+
+    //void match(DataSpot3D::DataSpot3DPtr spot_src, DataSpot3D::DataSpot3DPtr spot_target, std::vector<cv::DMatch>& matches);
+
+
+private:
+
+    // Match feature points using symmetry test and RANSAC
+    // returns fundemental matrix
+    cv::Mat match(cv::Mat& image1,
+                  cv::Mat& image2, // input images
+                  // output matches and keypoints
+                  std::vector<cv::DMatch>& matches,
+                  std::vector<cv::KeyPoint>& keypoints1,
+                  std::vector<cv::KeyPoint>& keypoints2);
+
+
+    // Clear matches for which NN ratio is > than threshold
+    // return the number of removed points
+    // (corresponding entries being cleared,
+    // i.e. size will be 0)
+    int ratioTest(std::vector<std::vector< cv::DMatch> >
+                  &matches);
+
+    // Insert symmetrical matches in symMatches vector
+    void symmetryTest(
+            const std::vector<std::vector< cv::DMatch> >& matches1,
+            const std::vector<std::vector< cv::DMatch> >& matches2,
+            std::vector<cv::DMatch>& symMatches);
+
+    // Identify good matches using RANSAC
+    // Return fundemental matrix
+    cv::Mat ransacTest(
+            const std::vector<cv::DMatch>& matches,
+            const std::vector<cv::KeyPoint>& keypoints1,
+            const std::vector<cv::KeyPoint>& keypoints2,
+            std::vector<cv::DMatch>& outMatches);
+
+
+
+    // pointer to the feature point detector object
+    cv::Ptr<cv::FeatureDetector> detector_;
+    // pointer to the feature descriptor extractor object
+    cv::Ptr<cv::DescriptorExtractor> extractor_;
+    float ratio_; // max ratio between 1st and 2nd NN
+    bool refineF_; // if true will refine the F matrix
+    double distance_; // min distance to epipolar
+    double confidence_; // confidence level (probability)
+
 };
 
 
